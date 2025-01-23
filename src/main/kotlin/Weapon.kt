dd01aaf96b98f2dd365d07654240eb935a6f03c1
Dhionatã <xiyunagi@gmail.com>
2025-01-23 14:13:31 -0300

    updateTTK() agora não faz arredondamento; toString() agora faz os arredondamentos só para apresentação, assim como acontece no jogo; Alterado cadência base da "FN SCAR-H", a mesma teve um nerf.

--- src/main/kotlin/Weapon.kt
+++ src/main/kotlin/Weapon.kt
@@ -19,8 +19,15 @@ data class Weapon(
         }
 
     override fun toString(): String {
-        return "Nome: $name | Dano: $damage | Cadência: $fireRate | Cabeça X $headMultiplier | Corpo X $bodyMultiplier | TTK[Tiro(s) em Tempo(s)]: Cabeça[${ttk.first().first}" +
-                " em ${ttk.first().second}], Corpo[${ttk.elementAt(1).first} em ${ttk.elementAt(1).second}], Média[${ttk.last().first} em ${ttk.last().second}]"
+        return "Nome: $name | Dano: $damage | Cadência: $fireRate | Cabeça X $headMultiplier | Corpo X ${
+            BigDecimal(bodyMultiplier).setScale(2, RoundingMode.HALF_UP)
+        } | TTK[Tiro(s) em Tempo(s)]: Cabeça[${ttk.first().first} em ${
+            BigDecimal(ttk.first().second).setScale(
+                3, RoundingMode.HALF_UP
+            )
+        }], Corpo[${ttk.elementAt(1).first} em ${
+            BigDecimal(ttk.elementAt(1).second).setScale(3, RoundingMode.HALF_UP)
+        }], Média[${ttk.last().first} em ${BigDecimal(ttk.last().second).setScale(3, RoundingMode.HALF_UP)}]"
     }
 
     /**
@@ -58,13 +65,13 @@ data class Weapon(
         ttk.clear()
 
         val headPair = TTKCalculator.calculateTTKWithProtection(this, set, true)
-        ttk.add(Pair(headPair.first, BigDecimal(headPair.second).setScale(5, RoundingMode.UP).toDouble()))
+        ttk.add(Pair(headPair.first, headPair.second))
 
         val bodyPair = TTKCalculator.calculateTTKWithProtection(this, set, false)
-        ttk.add(Pair(bodyPair.first, BigDecimal(bodyPair.second).setScale(5, RoundingMode.UP).toDouble()))
+        ttk.add(Pair(bodyPair.first, bodyPair.second))
 
         val ttkAverage = (headPair.second + bodyPair.second) / 2
-        ttk.add(Pair(((headPair.first + bodyPair.first) / 2), BigDecimal(ttkAverage).setScale(5, RoundingMode.UP).toDouble()))
+        ttk.add(Pair(((headPair.first + bodyPair.first) / 2), ttkAverage))
 
     }
 
@@ -79,14 +86,14 @@ data class Weapon(
             Weapon("AK Alpha (Mod RAJADA, Cadência e Dano)", 100, 800.0, 6.5, 1.0).addMods(-35.0, 50).addMods(6.0).addMods(damageAdd = 4),
             Weapon("AK-12 (Mod Cadência)", 105, 735.0, 7.0, 1.25).addMods(10.0),
             Weapon("Beretta (Mod Cadência)", 111, 810.0, 4.0, 1.4).addMods(10.0),
-            Weapon("Carmel (Mod Dano, Cadência e Corporal)", 96, 720.0, 7.0, 1.07).addMods(-27.5, 70, bodyMultiplierAddPercentage = 12.0).addMods(6.0).addMods
-                (bodyMultiplierAddPercentage = 12.0),
+            Weapon("Carmel (Mod Dano, Cadência e Corporal)", 96, 720.0, 7.0, 1.07).addMods(-27.5, 70, bodyMultiplierAddPercentage = 12.0).addMods(6.0)
+                .addMods(bodyMultiplierAddPercentage = 12.0),
             Weapon("Carmel (Mod Cadência [normal e especial] e Corporal)", 96, 720.0, 7.0, 1.07).addMods(45.0, headMultiplierAddPercentage = -45.0).addMods(6.0)
                 .addMods(bodyMultiplierAddPercentage = 12.0),
             Weapon("Cobalt (Mod Cadência [normal], +2 Corporal)", 95, 735.0, 7.0, 1.05).addMods(5.2).addMods(bodyMultiplierAddPercentage = 10.0)
                 .addMods(bodyMultiplierAddPercentage = 40.0),
-            Weapon("Cobalt (Mod Cadência [especial] e Corporal", 95, 735.0, 7.0, 1.05).addMods(40.0, headMultiplierAddPercentage = -32.0).addMods(5.2).addMods
-                (bodyMultiplierAddPercentage = 10.0),
+            Weapon("Cobalt (Mod Cadência [especial] e Corporal", 95, 735.0, 7.0, 1.05).addMods(40.0, headMultiplierAddPercentage = -32.0).addMods(5.2)
+                .addMods(bodyMultiplierAddPercentage = 10.0),
             Weapon("Kord", 175, 640.0, 6.0, 1.15),
             Weapon("Kord (Mod Cadência)", 175, 640.0, 6.0, 1.15).addMods(10.0),
             Weapon("Kord (Mod Cadência e Recuo)", 175, 640.0, 6.0, 1.15).addMods(10.0).addMods(-26.0),
@@ -98,7 +105,7 @@ data class Weapon(
             Weapon("STK (Mod Cadência e Corporal)", 110, 840.0, 4.0, 1.25).addMods(8.0, bodyMultiplierAddPercentage = 13.0),
             Weapon("STK (Mod Cadência, Corporal e Dano)", 110, 840.0, 4.0, 1.25).addMods(8.0).addMods(bodyMultiplierAddPercentage = 13.0)
                 .addMods(-42.0, 60, bodyMultiplierAddPercentage = -10.0),
-            Weapon("FN SCAR-H (Mod Cadência)", 175, 600.0, 7.0, 1.24).addMods(10.0),
+            Weapon("FN SCAR-H (Mod Cadência)", 175, 570.0, 7.0, 1.24).addMods(10.0),
             Weapon("AN-94 (Mod Cadência)", 125, 620.0, 7.0, 1.8).addMods(10.0),
             Weapon("MPAR-556 (Mod Cadência)", 110, 825.0, 4.0, 1.45).addMods(10.0),
             Weapon("MPAR-556", 110, 825.0, 4.0, 1.45),
@@ -122,18 +129,16 @@ data class Weapon(
 
         val pistolas: List<Weapon> = listOf(
             Weapon("Taurus Raging Hunter (Mod Cadência)", 350, 160.0, 6.0, 1.10).addMods(5.0),
-            Weapon("Taurus Raging Hunter (Mod Dano Corporal, Precisão e Cadência)", 350, 160.0, 6.0, 1.10).addMods(5.0).addMods(bodyMultiplierAddPercentage = 10.0)
-                .addMods(-10.0),
+            Weapon("Taurus Raging Hunter (Mod Dano Corporal, Precisão e Cadência)", 350, 160.0, 6.0, 1.10).addMods(5.0).addMods(bodyMultiplierAddPercentage = 10.0).addMods(-10.0),
             Weapon("Taurus Raging Hunter (Mod Dano Corporal e Cadência)", 350, 160.0, 6.0, 1.10).addMods(5.0).addMods(bodyMultiplierAddPercentage = 10.0),
-            Weapon("Taurus Raging Hunter (Mod Dano Corporal, Cadência e Cadência [especial])", 350, 160.0, 6.0, 1.10).addMods(5.0)
-                .addMods(bodyMultiplierAddPercentage = 10.0)
+            Weapon("Taurus Raging Hunter (Mod Dano Corporal, Cadência e Cadência [especial])", 350, 160.0, 6.0, 1.10).addMods(5.0).addMods(bodyMultiplierAddPercentage = 10.0)
                 .addMods(55.0, -90),
             Weapon("SIG Sauer P226 (Mod Cadência e Dano)", 155, 275.0, 4.0, 1.05).addMods(8.0).addMods(damageAdd = 26),
             Weapon("SIG Sauer P226 (Mod Cadência, Dano e Precisão)", 155, 275.0, 4.0, 1.05).addMods(8.0).addMods(damageAdd = 26).addMods(-30.0, 80),
             Weapon("Maxim 9", 130, 290.0, 3.5, 1.15),
             Weapon("ST Kinetics (Mod Cadência e Corporal", 108, 880.0, 5.0, 1.1).addMods(12.0).addMods(bodyMultiplierAddPercentage = 16.0),
-            Weapon("ST Kinetics (Mod Cadência, Corporal e Dupla", 108, 880.0, 5.0, 1.1).addMods(12.0).addMods(bodyMultiplierAddPercentage = 16.0).addMods(7.0, -15, -20.0).addMods
-                (100.0),
+            Weapon("ST Kinetics (Mod Cadência, Corporal e Dupla", 108, 880.0, 5.0, 1.1).addMods(12.0).addMods(bodyMultiplierAddPercentage = 16.0).addMods(7.0, -15, -20.0)
+                .addMods(100.0),
             Weapon("Taurus Judge (Mod Cadência)", 680, 100.0, 3.0, 1.4).addMods(10.0)
         )
     }
