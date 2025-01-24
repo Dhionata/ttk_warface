2df375ca76f109e2cd3d7185afb070172a9c89a2
Dhionatã <xiyunagi@gmail.com>
2025-01-24 12:17:00 -0300

    Substituído elementAt() por []; Adicionado "Tempo médio de resistência" para o conjunto.

--- src/main/kotlin/WeaponPresenter.kt
+++ src/main/kotlin/WeaponPresenter.kt
@@ -1,5 +1,8 @@
 package br.com.dhionata
 
+import java.math.BigDecimal
+import java.math.RoundingMode
+
 object WeaponPresenter {
 
     fun printDetailedAllWeaponsInfo(fuzileiroWeapons: List<Weapon>, engenheiroWeapons: List<Weapon>, pistolas: List<Weapon>, set: Set, debug: Boolean = false) {
@@ -8,13 +11,13 @@ object WeaponPresenter {
         println("\n==== TTK no corpo ====")
 
         println("\n=== Classe Fuzileiro ===\n")
-        fuzileiroWeapons.sortedBy { it.ttk.elementAt(1).second }.forEach { println(it) }
+        fuzileiroWeapons.sortedBy { it.ttk[1].second }.forEach { println(it) }
 
         println("\n=== Classe Engenheiro ===\n")
-        engenheiroWeapons.sortedBy { it.ttk.elementAt(1).second }.forEach { println(it) }
+        engenheiroWeapons.sortedBy { it.ttk[1].second }.forEach { println(it) }
 
         println("\n=== Pistolas ===\n")
-        pistolas.sortedBy { it.ttk.elementAt(1).second }.forEach { println(it) }
+        pistolas.sortedBy { it.ttk[1].second }.forEach { println(it) }
 
         println("\n==== TTK na cabeça ====")
 
@@ -42,7 +45,7 @@ object WeaponPresenter {
         (fuzileiroWeapons + engenheiroWeapons).sortedBy { it.ttk.first().second }.forEach { println(it) }
 
         println("\n=== Fuzileiro + Engenheiro ===\n\n==== TTK no corpo ====\n")
-        (fuzileiroWeapons + engenheiroWeapons).sortedBy { it.ttk.elementAt(1).second }.forEach { println(it) }
+        (fuzileiroWeapons + engenheiroWeapons).sortedBy { it.ttk[1].second }.forEach { println(it) }
 
         println("\n=== Fuzileiro + Engenheiro ===\n\n==== TTK Médio ====\n")
         (fuzileiroWeapons + engenheiroWeapons).sortedBy { it.ttk.last().second }.forEach { println(it) }
@@ -55,5 +58,27 @@ object WeaponPresenter {
         println("\n=== Classe Engenheiro ===\n")
         TTKCalculator.findBestTTK(engenheiroWeapons, set, debug).toList().forEach { TTKCalculator.printWeaponTTKWithProtection(it, set) }
 
+        println("\n==== Tempo médio de resistência contra Fuzi. + Eng. com o conjunto ${set.name}")
+
+        println("\n=== Cabeça ===")
+
+        println("\n== Tiros ==")
+        val mediaDeTirosNaCabeca = (fuzileiroWeapons + engenheiroWeapons).map { it.ttk.first().first }.average()
+        println(BigDecimal(mediaDeTirosNaCabeca).setScale(2, RoundingMode.HALF_UP))
+
+        println("\n== Tempo ==")
+        val tempoMedioDeResistenciaCabeca = (fuzileiroWeapons + engenheiroWeapons).map { it.ttk.first().second }.average()
+        println(BigDecimal(tempoMedioDeResistenciaCabeca).setScale(2, RoundingMode.HALF_UP))
+
+
+        println("\n=== Corpo ===")
+
+        println("\n== Tiros ==")
+        val mediaDeTirosNaCorpo = (fuzileiroWeapons + engenheiroWeapons).map { it.ttk[1].first }.average()
+        println(BigDecimal(mediaDeTirosNaCorpo).setScale(2, RoundingMode.HALF_UP))
+
+        println("\n== Tempo ==")
+        val tempoMedioDeResistenciaCorpo = (fuzileiroWeapons + engenheiroWeapons).map { it.ttk[1].second }.average()
+        println(BigDecimal(tempoMedioDeResistenciaCorpo).setScale(2, RoundingMode.HALF_UP))
     }
 }
